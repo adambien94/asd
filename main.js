fd892f2df0c9f578555763dd08abf4b561de7693
Adam Bien <adam.bien94@gmail.com>
2019-03-31 16:44:46 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+$(window).scroll(function() {
+  if (window.scrollY > $("header").innerHeight() - 50) {
+    $("#nav-id").addClass("nav-shrinked");
+  } else {
+    $("#nav-id").removeClass("nav-shrinked");
+  }
+});
+
+$("#hamburger").click(function() {
+  navToggle();
+});
+
+$("#blackout").click(function() {
+  navHide();
+});
+
+$(".nav-link").click(function() {
+  navHide();
+});
+
+$(".home-link").click(function() {
+  navHide();
+});
+
+function navToggle() {
+  $("#nav-ul").toggleClass("nav-ul-opened");
+  $("#blackout").toggleClass("blackout-on");
+  $("#nav-id").toggleClass("nav-opened");
+  $(document.body).toggleClass("no-scroll");
+}
+
+function navHide() {
+  $("#nav-ul").removeClass("nav-ul-opened");
+  $("#blackout").removeClass("blackout-on");
+  $("#nav-id").removeClass("nav-opened");
+  $(document.body).removeClass("no-scroll");
+}
